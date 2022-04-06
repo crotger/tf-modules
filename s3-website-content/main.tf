cca7fc1ccd1898847993f96d5649fc51a81f8a7a
Carlos Rotger <carlosrotger42@gmail.com>
2022-04-06 10:18:31 -0700

    s3-website-content module

---
+++ s3-website-content/main.tf
@@ -0,0 +1,48 @@
+variable "content_folder" {
+  type = string
+  description = "contents to upload to S3"
+}
+
+variable "s3_root_path" {
+  type = string
+  description = "root folder to upload to in S3"
+  default = ""
+}
+
+variable "bucket" {
+  type = any
+}
+
+locals {
+  local_content_base  = abspath(var.content_folder)
+  remote_content_base = var.s3_root_path
+  files = fileset(local.local_content_base, "**")
+
+  mime_types = {
+    ".html" = "text/html"
+    ".json" = "application/json"
+    ".png"  = "image/png"
+    ".css"  = "text/css"
+    ".js"   = "application/javascript"
+  }
+
+  # default_mime_type = "binary/octet-stream"
+}
+
+resource "aws_s3_bucket_object" "website_files" {
+  for_each = local.files
+  bucket = var.bucket.bucket
+  key = "${local.remote_content_base}/${each.value}"
+  source = "${local.local_content_base}/${each.value}"
+
+  etag = filemd5("${local.local_content_base}/${each.value}")
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
+}
+
+output "files" {
+  value = local.files
+}
+
+output "objects" {
+  value = aws_s3_bucket_object.website_files.*
+}
