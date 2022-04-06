61508046b1c7d539da471064b61dca5aa1598971
Carlos Rotger <carlosrotger42@gmail.com>
2022-04-06 10:18:13 -0700

    s3-website module

---
+++ s3-website/tls.tf
@@ -0,0 +1,50 @@
+resource "aws_cloudfront_distribution" "website" {
+  enabled = true
+
+  aliases = [var.domain_name]
+
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id    = "S3Static"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.website_s3_access.cloudfront_access_identity_path
+    }
+  }
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id = "S3Static"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = var.certificate.arn
+    ssl_support_method  = "sni-only"
+    # cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "website_s3_access" {
+  comment = "Access to ${var.domain_name} website bucket"
+}
